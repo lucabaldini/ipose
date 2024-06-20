1c90078dd9b53a9aa05fa008d75ab838648d44d5
Luca Baldini <luca.baldini@pi.infn.it>
2024-06-20 07:32:09 +0200

    Minor.

--- ipose/opts.py
+++ ipose/opts.py
@@ -13,46 +13,62 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+"""Command-line options.
+"""
 
 import argparse
+import typing
 
 from ipose import IPOSE_DATA
 
+
+
 _OPTION_DICT = {
-    '--scale-factor': dict(type=float, default=1.1,
+    # Face detection: opencv setings.
+    'scale-factor': dict(type=float, default=1.1,
         help='scaleFactor parameter for the opencv detectMultiScale() call, '
              'determining how much the image size is reduced at each image scale'),
-    '--min-neighbors': dict(type=int, default=2,
+    'min-neighbors': dict(type=int, default=2,
         help='minNeighbors parameter for the opencv detectMultiScale() call, '
              'determining how many neighbors each candidate rectangle should '
              'have to retain it'),
-    '--min-size': dict(type=float, default=0.175,
+    'min-size': dict(type=float, default=0.175,
         help='minimum rectangle size as a fraction of the effective size of the '
             'original image'),
-
-    '--horizontal-padding': dict(type=float, default=0.4,
+    # Face cropping: basic appearance.
+    'horizontal-padding': dict(type=float, default=0.4,
         help='fractional padding in the horizontal direction for the best rectangle '
              'identified by the face-recognition step'),
-    '--top-scale-factor': dict(type=float, default=1.25,
+    'top-scale-factor': dict(type=float, default=1.25,
         help='scale factor for the top padding, relative to the horizontal padding '
              '(the padding on the bottom will be adjusted to keep the final image square)'),
-    '--output-size': dict(type=int, default=100,
+    'output-size': dict(type=int, default=100,
         help='size of the output (square) image in pixels'),
-    '--circular-mask': dict(action='store_true', default=False,
+    'circular-mask': dict(action='store_true', default=False,
         help='if set, a circular mask will be applied in the output image'),
-
-    '--output-folder': dict(type=str, default=IPOSE_DATA,
+    # Mass processing output options.
+    'output-folder': dict(type=str, default=IPOSE_DATA,
         help='path to the folder for the output files'),
-    '--suffix': dict(type=str, default=None,
+    'suffix': dict(type=str, default=None,
         help='optional suffix for the output files'),
-    '--interactive': dict(action='store_true', default=False,
+    'interactive': dict(action='store_true', default=False,
         help='run in interactive mode')
 }
 
 
 
-def default_value(key: str):
-    """
+def option_default_value(key: str) -> typing.Any:
+    """Return the default value for a given option.
+
+    Parameters
+    ----------
+    key
+        The optional argument name without the leading `--`.
+
+    Returns
+    -------
+    typing.Any
+        The default value for a given optional argument.
     """
     return _OPTION_DICT[key]['default']
 
@@ -73,41 +89,90 @@ class _Formatter(argparse.RawDescriptionHelpFormatter, argparse.ArgumentDefaults
 
 class MainArgumentParser(argparse.ArgumentParser):
 
-    """
+    """Application-wide argument parser.
     """
 
     _DESCRIPTION = None
     _EPILOG = None
     _FORMATTER_CLASS = _Formatter
-    _OPTS_FACE_DETECTION = ('--scale-factor', '--min-neighbors', '--min-size')
-    _OPTS_APPEARANCE = ('--horizontal-padding', '--top-scale-factor', '--output-size', '--circular-mask')
-    _OPTS_OUTPUT = ('--output-folder', '--suffix', '--interactive')
 
     def __init__(self) -> None:
         """Overloaded method.
         """
         super().__init__(description=self._DESCRIPTION, epilog=self._EPILOG,
             formatter_class=self._FORMATTER_CLASS)
-        self._add_group('face detection', *self._OPTS_FACE_DETECTION)
-        self._add_group('appearance', *self._OPTS_APPEARANCE)
-        self._add_group('output', *self._OPTS_OUTPUT)
+
+        subparsers = self.add_subparsers(help='sub-command help')
+        # See https://stackoverflow.com/questions/8757338/
+        subparsers._parser_class = argparse.ArgumentParser
+        parser_facecrop = subparsers.add_parser('facecrop', help='crop images to face')
+        self.add_face_detection_group(parser_facecrop)
+        self.add_appearance_group(parser_facecrop)
+        self.add_output_group(parser_facecrop)
+        parser_facetile = subparsers.add_parser('facetile', help='tile face images')
 
     @staticmethod
-    def _add_arguments_from_dict(parser, *keys: str) -> None:
-        """
+    def _add_options_from_dict(container: argparse._ActionsContainer, *keys: str) -> None:
+        """Add one or more optional arguments to a given container.
+
+        Parameters
+        ----------
+        container
+            The action container, typically the main OptionParser itself or a
+            child option group.
+
+        keys
+            The name of the optional argument(s) to be added, whose keyword arguments
+            must be defined in the `_OPTION_DICT` variable at the top of the module.
         """
         for key in keys:
-            parser.add_argument(key, **_OPTION_DICT[key])
+            container.add_argument(f'--{key}', **_OPTION_DICT[key])
 
-    def _add_group(self, name: str, *keys: str):
+    @staticmethod
+    def add_option_group(container: argparse._ActionsContainer, name: str,
+        *keys: str) -> argparse._ArgumentGroup:
+        """Add a new option group to the parser, based on the `_OPTION_DICT` variable
+        at the top of the module.
+
+        Parameters
+        ----------
+        name
+            The name of the option group.
+
+        keys
+            The name of the optional argument(s) to be added, whose keyword arguments
+            must be defined in the `_OPTION_DICT` variable at the top of the module.
+
+        Returns
+        -------
+        argparse._ArgumentGroup
+            The newly created group.
         """
-        """
-        group = self.add_argument_group(name)
-        self._add_arguments_from_dict(group, *keys)
+        group = container.add_argument_group(name)
+        MainArgumentParser._add_options_from_dict(group, *keys)
         return group
+
+    @staticmethod
+    def add_face_detection_group(container: argparse._ActionsContainer) -> None:
+        # pylint: disable=missing-function-docstring
+        keys = ('scale-factor', 'min-neighbors', 'min-size')
+        MainArgumentParser.add_option_group(container, 'face detection', *keys)
+
+    @staticmethod
+    def add_appearance_group(container: argparse._ActionsContainer) -> None:
+        # pylint: disable=missing-function-docstring
+        keys = ('horizontal-padding', 'top-scale-factor', 'output-size', 'circular-mask')
+        MainArgumentParser.add_option_group(container, 'appearance', *keys)
+
+    @staticmethod
+    def add_output_group(container: argparse._ActionsContainer) -> None:
+        # pylint: disable=missing-function-docstring
+        keys = ('output-folder', 'suffix', 'interactive')
+        MainArgumentParser.add_option_group(container, 'output', *keys)
 
 
 
 if __name__ == '__main__':
     parser = MainArgumentParser()
-    parser.parse_args()
+    args = parser.parse_args()
+    print(args)
